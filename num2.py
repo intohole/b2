166546b6583a50f59098a9b2055747dc701b591f
intoblack <intoblack86@gmail.com>
2014-09-04 22:16:42 +0800

    初次提交

---
+++ num2.py
@@ -0,0 +1,34 @@
+#coding=utf-8
+
+
+from random import randint
+
+
+
+def get_random_seq(seq_len):
+    rand_num = str(randint(0 , 10 ** (seq_len ) - 1))
+    seq_str = ['0' for _ in range(seq_len)]
+    for i in range(len(rand_num)):
+        seq_str[seq_len-i -1] = rand_num[i]
+    return ''.join(seq_str)
+
+
+def get_random_seq1(seq_len):
+    rand_num = str(randint (0 , 10 ** seq_len - 1))
+    return '%s%s' % (''.join(['0' for _ in range(seq_len - len(rand_num))] ), rand_num)
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    i ={3 : 2 , 2: 3}
+    c = i.items()
+    print c
+    print sorted(c ,key=  lambda x : x[0] , reverse = True)
+    print c
+    print get_random_seq1(5)
