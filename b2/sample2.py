96ee90c794bb9c3e41f5d55e2513a54fb47f4708
intoblack <intoblack86@gmail.com>
2014-10-11 22:36:18 +0800

    添加一个随机字符串生成的工具

--- b2/sample2.py
+++ b2/sample2.py
@@ -31,17 +31,48 @@ def get_rand(min_value , max_value , limit =10000):
 
 
         def next(self):
-            if self.__index == self.limit :
+            if limit and self.__index == self.limit :
                 raise StopIteration , 'iter bigger than limit! limit = %s'  % self.limit
             self.__index += 1
             return randint(self.min_value , max_value)
 
         def has_next(self):
-            if self.__index < self.limit:
-                return True
-            return False
+            if self.limit and self.__index < self.limit:
+                return False
+            return True
 
     return RandInt(min_value , max_value , limit)
+
+def rand_string(l  ,lower_str = True  , higher_str = True , num_str = True ,limit =1000000):
+
+    class  __RandString(object):
+        def __init__(self , string_len , limit , lower_str = True  , higher_str = True , num_str = True):
+            self.__char = []
+            if lower_str:
+                self.__char.extend([ chr(ord('a')+ i) for i in range(ord('z') - ord('a') + 1)])
+            if higher_str:
+                self.__char.extend([chr(ord('A')+ i) for i in range(ord('Z') - ord('A') + 1)])
+            if num_str:
+                self.__char.extend([str(i) for i in range(10)])
+            if len(self.__char) == 0:
+                raise ValueError , 'must set lower_str / higher_str /num_str more than one True'
+            self.__rand = get_rand(0 , len(self.__char) - 1  , None)
+            self.__index = 0
+            self.limit = limit
+            self.string_len = string_len
+
+        def next(self):
+            if limit and self.__index == self.limit :
+                raise StopIteration , 'iter bigger than limit! limit = %s'  % self.limit
+            self.__index += 1
+            return ''.join([self.__char[self.__rand.next()] for i in range(self.string_len)])
+
+        def has_next(self):
+            if self.limit and self.__index < self.limit:
+                return False
+            return True
+    return __RandString(l , limit)
+
 
 
 def reservoir(datas, k):
@@ -65,14 +96,21 @@ def reservoir(datas, k):
                 data[swap] = datas[i]
         return data
 
+
+
+
+
+
 if __name__ == '__main__':
-    datas = [ i  for i in range(100000)]
-    # print reservoir(datas , 1000)
-    a = iter(get_rand(1, 10))
-    for i in a:
-        print i
+    # datas = [ i  for i in range(100000)]
+    # # print reservoir(datas , 1000)
+    # a = iter(get_rand(1, 1000))
+    # for i in a:
+    #     print i
+    a = rand_string(10 , 1000)
+    print a.next()
 
 
-    print a.next()
-    print a.next()
-    print a.next()
+    # print a.next()
+    # print a.next()
+    # print a.next()
