e301ce6939bd6a6ba5a393d36794b15420a07fc9
intoblack <intoblack86@gmail.com>
2014-11-25 22:16:12 +0800

    打算写个hadoop封装命令 让别人更简单的使用

---
+++ test/testHadoop2.py
@@ -0,0 +1,40 @@
+#coding=utf-8
+
+
+import os
+
+
+
+
+
+class Utils(object):
+
+
+
+    @staticmethod
+    def file_filter(filter = None , path = os.path.dirname(__file__)  ):
+        for f in os.listdir(path):
+
+
+
+
+
+
+class Command(dict):
+    pass
+
+
+
+
+
+
+class HadoopCommand(object):
+
+
+
+    def __init__(self , hadoop_home = os.getenv('HADOOP_HOME')):
+        if not ( hadoop_home and os.path.isdir(hadoop_home)):
+            raise ValueError , 'HADOOP_HOME path does\'nt exist ! %s ' % hadoop_home
+        self.hadoop_home = hadoop_home
+
+
