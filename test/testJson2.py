7bc6de04fbd57ff11b9ec4224184d2fdf7627f02
intoblack <intoblack86@gmail.com>
2015-12-27 10:27:13 +0800

    提交测试testJson2.py代码

--- test/testJson2.py
+++ test/testJson2.py
@@ -2,7 +2,7 @@
 
 
 import json
-
+import re
 
 class JsonXpath(object):
 
@@ -26,10 +26,17 @@ class JsonXpath(object):
             for _obj in obj:
                 self._finds_name(tag , _obj , container)
         elif isinstance(obj , dict):
-            if tag in obj:
-                container.append(obj[tag])
+            self.regx_find_dict(tag , obj , container)
             for key , _obj in obj.items():
                 self._finds_name( tag , _obj  , container)
+        return 
+
+    def regx_find_dict(self , tag , obj , container):
+        tag_pattern = re.compile(tag)
+        for name , value in obj.items(): 
+            match = tag_pattern.match(name)
+            if match:
+                container.append(value)
         return 
 
     def _find_name(self , tag , obj ):
@@ -41,6 +48,44 @@ if __name__ == "__main__":
 
     x = JsonXpath('{"a":"c"}')
     obj = {"a" :[{"b":{"c":1}} , {"b":{"c" , 2}}]}
+    obj = json.loads('''{
+    "store": {
+        "book": [
+            {
+                "category": "reference",
+                "author": "Nigel Rees",
+                "title": "Sayings of the Century",
+                "price": 8.95
+            },
+            {
+                "category": "fiction",
+                "author": "Evelyn Waugh",
+                "title": "Sword of Honour",
+                "price": 12.99
+            },
+            {
+                "category": "fiction",
+                "author": "Herman Melville",
+                "title": "Moby Dick",
+                "isbn": "0-553-21311-3",
+                "price": 8.99
+            },
+            {
+                "category": "fiction",
+                "author": "J. R. R. Tolkien",
+                "title": "The Lord of the Rings",
+                "isbn": "0-395-19395-8",
+                "price": 22.99
+            }
+        ],
+        "bicycle": {
+            "color": "red",
+            "price": 19.95
+        }
+        },
+    "expensive": 10
+    }''')
     container = []
-    x._finds_name("b" , obj , container)
+
+    x._finds_name("author" , obj , container)
     print container
