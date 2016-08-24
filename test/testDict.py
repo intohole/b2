5921f6342d2fac3a6e1061664cf9af8630b1e2c9
慕江 <xuze.lxz@alibaba-inc.com>
2016-08-24 20:10:13 +0800

    a

--- test/testDict.py
+++ test/testDict.py
@@ -1,17 +1,48 @@
 #coding=utf-8
 
+import threading
 
-def update_config(config , **kw):
-    for key , val in kw.items():
-        if key in config.keys():
-            config[key] = val 
-    return config 
+class DataStamp(object):
 
+    def __init__(self , timestamp , key):
+        self.timestamp = timestamp 
+        self.key = key 
 
-a = {'a' : 1 }
+class StampList(object):
+    
+    def __init__(self):
+        self.l = []
+    
+    def add(self , datastamp):
+        self.l.insert() 
+    
+class TairDict(threading.Thread,dict):
 
+    def __init__(self):
+        super(TairDict,self).__init__()
+        self.start() 
 
-update_config(a , a='2' , b ='2')
+    def run(self):
+        print "i'm run "
 
-
-print a 
+    def __setitem__(self, key , value):
+        if key in self:
+            pass
+    
+    def put(self , key , value , endTime = None):
+        pass
+ 
+    def __getitem__(self , key):
+        if key in self:
+            value = super(TairDict , self).__getitem__(key) 
+            if value.timestamp > long(time.time()):
+                return None
+            else:
+                return value.value 
+        else:
+            return None 
+    
+    def get(self , key):
+        pass
+if __name__ == "__main__":
+    t = TairDict()
