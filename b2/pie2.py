ff0b2e63c9413d5b50692b777af96e5603cc0248
intohole <intoblack86@gmail.com>
2016-09-15 12:06:49 +0800

    pipe

--- b2/pie2.py
+++ b2/pie2.py
@@ -3,5 +3,38 @@
 
 import subprocess
 
-def run_cmd():
-    pass
+class PipeCommandItem(object):
+
+
+    def __init__(self , command ,shell = False):
+        if command and isinstance(command,(basestring ,tuple ,list)):
+            self.command = command
+        else:
+            raise TypeError
+        self.shell = shell
+
+class PipeCommand(object):
+
+
+    def __init__(self):
+        self._commands = []
+
+    def add(self,command):
+        pass
+def _check_pipe_items(pipe_commands):
+    if pipe_commands and hasattr(pipe_commands):
+        for pipe_command in pipe_commans:
+            if isinstance(pipe_command,PipeCommandItem) is False:
+                raise TypeError
+        return
+    raise ValueError
+
+def run_pipe(pipe_commands):
+    self._check_pipe_items(pipe_commands)
+    pipes = []
+    for pipe_command in pipe_commands:
+        stdin = subprocess.PIPE.stdin
+        if len(pipes) != 0:
+            stdin = pipes[-1]
+        pipes.append(subprocess.Popen(pipe_command.command,shell=pipe_command.shell,stdin = stdin,stdout=subprocess.PIPE.stdout))
+    pipes[-1].communicate()
