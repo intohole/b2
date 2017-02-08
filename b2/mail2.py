4acb63edfde159b4d1415fc307627c3d5bf7f214
intohole <intoblack86@gmail.com>
2017-02-08 19:56:25 +0800

    change extract file

--- b2/mail2.py
+++ b2/mail2.py
@@ -17,17 +17,17 @@ SERVER_DICT = object2.StaticDict({"126.com":"smtp.126.com"})
 
 def mail( user , pass_wd  ,title , content , tos , server = None,port = 25, files = None , html = False ,encoding = None):
     if isinstance(tos , basestring):
-        tos = [tos] 
+        tos = [tos]
     user , suffix = user.split("@")
-    server = SERVER_DICT.get(suffix) if server is None else server  
+    server = SERVER_DICT.get(suffix) if server is None else server
     exceptions2.judge_str(server)
-    exceptions2.judge_type(tos , (list , tuple)) 
+    exceptions2.judge_type(tos , (list , tuple))
     content_type = "plain" if html is False else "html"
     encoding = "utf-8" if encoding is None else encoding
-    mail_object = MIMEText(content , _subtype = content_type ,_charset = encoding) 
-    mail_object["Subject"] = title 
-    mail_object["From"] = "{user}<{user}@{suffix}>".format(user = user , suffix = suffix) 
-    mail_object["To"] = ";".join(tos) 
+    mail_object = MIMEText(content , _subtype = content_type ,_charset = encoding)
+    mail_object["Subject"] = title
+    mail_object["From"] = "{user}<{user}@{suffix}>".format(user = user , suffix = suffix)
+    mail_object["To"] = ";".join(tos)
     service = smtplib.SMTP(server , port)
     service.login(user , pass_wd)
     service.sendmail("{user}@{suffix}".format(user = user , suffix = suffix ) , tos , mail_object.as_string())
--- extract.sh
+++ extract.sh
@@ -8,5 +8,9 @@ cd `dirname $0`
 CWD=`pwd`
 for function_file in `ls ${CWD}/b2/*.py`;do
     echo "+ `basename ${function_file}`" >>function.md
-    cat ${function_file} | grep -E "(^class|def )" | grep -v "__" | sed  's/class /    + /' | sed 's/def /    + /' | sed 's/:$//' >> function.md 
+    cat ${function_file} | grep -En "(^class|def )" | grep -v "__" | sed  's/class /    + /' | sed 's/def /    + /' | sed 's/:$//' | awk -F ":" '{
+    line_number = $1;
+    function_name = $2;
+    print function_name"](https://github.com/intohole/b2/blob/master/b2/cache2.py#L"line_number")" 
+}' | sed 's/+ /+ [/'>> function.md 
 done
