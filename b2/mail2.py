bca0c3aba82cea7e12f6a2fefdea5b7b10c95d3f
intohole <intoblack86@gmail.com>
2017-04-30 16:39:36 +0800

    添加邮件可以传送附件功能，标题unicode化

--- b2/mail2.py
+++ b2/mail2.py
@@ -5,9 +5,14 @@
 import object2
 import smtplib
 from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from os.path import basename
 import exceptions2
 import object2
+import system2
 
+system2.reload_utf8()
 
 __ALL__ = ["mail"]
 
@@ -24,10 +29,23 @@ def mail( user , pass_wd  ,title , content , tos , server = None,port = 25, file
     exceptions2.judge_type(tos , (list , tuple))
     content_type = "plain" if html is False else "html"
     encoding = "utf-8" if encoding is None else encoding
-    mail_object = MIMEText(content , _subtype = content_type ,_charset = encoding)
+    mail_object = MIMEMultipart()
+    if not isinstance(title,unicode):
+        title = unicode(title)
     mail_object["Subject"] = title
     mail_object["From"] = "{user}<{user}@{suffix}>".format(user = user , suffix = suffix)
     mail_object["To"] = ";".join(tos)
+    mail_object.attach(MIMEText(content, _subtype = content_type , _charset = encoding))
+
+    for f in files or []:
+        with open(f, "rb") as fil:
+            part = MIMEApplication(
+                fil.read(),
+                Name=basename(f)
+            )
+            part['Content-Disposition'] = 'attachment; filename="%s"' % basename(f)
+            mail_object.attach(part)
     service = smtplib.SMTP(server , port)
     service.login(user , pass_wd)
     service.sendmail("{user}@{suffix}".format(user = user , suffix = suffix ) , tos , mail_object.as_string())
+
